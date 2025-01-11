210a115f0e078297118ccda0fa5d085d37fe4cea
Djurinschi Andrei <djurinschi.andreiai@gmail.com>
2025-01-12 01:46:31 +0200

    SearchBar function + Details pagr

--- TeachSyncApp/Controllers/User/UserController.cs
+++ TeachSyncApp/Controllers/User/UserController.cs
@@ -10,11 +10,20 @@ namespace TeachSyncApp.Controllers.User;
 public class UserController(ApplicationDbContext context) : Controller
 {
     [HttpGet]
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string? searchName)
     {
-        var users = context.Users.Include(u => u.Role);
-        return View(await users.ToListAsync());
+        ViewData["SearchName"] = searchName;
+        if (string.IsNullOrEmpty(searchName))
+        {
+            var users = context.Users.Include(u => u.Role);
+            return View(await users.ToListAsync());
+        }
+        var sortedUsers = context.Users.Include(u => u.Role)
+            .Where(u => u.Name.ToLower().Contains(searchName.ToLower()) || u.Surname.ToLower().Contains(searchName.ToLower()));
+        return View(await sortedUsers.ToListAsync());
     }
+
+
 
     [HttpGet]
     public async Task<IActionResult> Details(int? id) //получаю только 1 пользователя
@@ -59,6 +68,40 @@ public class UserController(ApplicationDbContext context) : Controller
         await context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
     }
+
+    [HttpGet]
+    public IActionResult Edit()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int? id, UserViewModel modelUser)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        if(!ModelState.IsValid)
+        {
+            return View(modelUser);
+        }
+        user.Name = modelUser.Name;
+        user.Surname = modelUser.Surname;
+        user.Email = modelUser.Email;
+        user.RoleId = modelUser.RoleId;
+        await context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+ 
 
 
 
--- TeachSyncApp/obj/Debug/net9.0/TeachSyncApp.AssemblyInfo.cs
+++ TeachSyncApp/obj/Debug/net9.0/TeachSyncApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeachSyncApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a722f9052e0916f94a5813f2cbecf0d37c35030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc8d1b3e14cc7bc94692365a1b21503deeed650")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeachSyncApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeachSyncApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
